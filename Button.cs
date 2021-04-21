09db46608dbdffbe3c64aa2ded45961e9b528f17
Greg Fominykh <63728373+fominykhgreg@users.noreply.github.com>
2021-04-21 15:45:14 +0500

    scripts

---
+++ Button.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Button : MonoBehaviour
+{
+    public static bool trigger = false;
+    public void startoletka()
+    {
+        Handheld.Vibrate();
+        if (trigger == true)
+        {
+            trigger = false;
+        }
+        else 
+        {
+            trigger = true;            
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TImer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TImer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //private DateTime StartTime;
+    static bool trigger1 = Button.trigger;
+
+    public float time = 0;
+    Text life;
+
+    void Start()
+    {
+        life = GetComponent<Text>();
+    }
+   
+    // Update is called once per frame
+
+    void Update()
+    {
+        
+        if (Button.trigger == true)
+        {
+            time += Time.deltaTime;
+            
+            life.text = "" + time.ToString("00.00"); 
+
+
+        }
+        if (reset_button.reset_trigger == 1)
+        {
+            time = 0;
+            reset_button.reset_trigger = 0;
+            life.text = "" + time.ToString("00.00");
+            Button.trigger = false;
+        }
+    }
+}
